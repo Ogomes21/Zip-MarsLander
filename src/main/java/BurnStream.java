c0dbd6af831a5be379fe2618ebc6d2fc7ba9328c
octavia <Ogomes21@gmail.com>
2022-07-24 16:53:38 -0400

    Tried a bunch of things.

--- src/main/java/BurnStream.java
+++ src/main/java/BurnStream.java
@@ -1,3 +1,9 @@
 public interface BurnStream {
     int getNextBurn(DescentEvent status);
+
+    public static void main(String[] args) {
+      Simulation game = new Simulation(new Vehicle(Simulation.randomaltitude()));
+        BurnStream burnSource = new BurnInputStream();
+        game.runSimulation(burnSource);
+    }
 }
--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -3,9 +3,20 @@ public class OnBoardComputer implements BurnStream {
     @Override
     public int getNextBurn(DescentEvent status) {
         int burn = 0;
-        
+
         System.out.println(burn); /*hack!*/
         return burn;
-    }
 
+//        int a = status.getAltitude();
+//        int v = status.getVelocity();
+//        int burn = 100;
+//        if (a >1000 && v > 100) {
+//            burn = 170;
+//            burn *=10;
+//        }
+//        burn = (v*v)/(2*a);
+//        System.out.println(burn); /*hack!*/
+//        return burn;
+//        }
+    }
 }
